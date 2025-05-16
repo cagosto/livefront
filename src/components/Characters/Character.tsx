ff7ad331969026c21d81ebc650da159b1a7f3209
Carlos Agosto <carlos.agosto@gmail.com>
2025-05-16 14:25:57 -0400

    feat update character search

--- src/components/Characters/Character.tsx
+++ src/components/Characters/Character.tsx
@@ -1,14 +1,31 @@
 import { Character } from '@/interfaces/characters';
 import Image from 'next/image';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
 
 interface CharacterProps {
   character: Character;
+  searchTerm: string;
 }
-export default function CharacterItem({ character }: CharacterProps) {
+export default function CharacterItem({
+  character,
+  searchTerm,
+}: CharacterProps) {
+  const matches = match(character.name, searchTerm, { insideWords: true });
+  const parts = parse(character.name, matches);
   return (
     <div className="bg-secondary rounded p-2 relative overflow-hidden group">
       <div className="absolute scale-x-0 bottom-0 top-0 p-1 bg-accent/80 right-0 left-0 transition group-hover:scale-x-100 origin-right group-hover:origin-left">
-        <h2>{character.name}</h2>
+        <h2>
+          {parts.map((parts, index) => (
+            <span
+              className={`${parts.highlight ? 'font-bold' : 'font-normal'}`}
+              key={index}
+            >
+              {parts.highlight}
+            </span>
+          ))}
+        </h2>
         <hr />
         <ul>
           <li>Actor: {character.actor}</li>
